68244a180aad895df0cdeac6ca5d32b2d192d6cf
tt70629 <tt70629@icloud.com>
2021-07-25 20:58:28 +1000

    Relocate code and add new functionality 1.Relocate the post request to page component. 2.Add functionality of liking a post then disable the like button.

--- BlogApp/src/components/PostItem.jsx
+++ BlogApp/src/components/PostItem.jsx
@@ -3,16 +3,23 @@ import {Card, Button} from 'react-bootstrap';
 
 
 const PostItem = (props) => {
+    const updateLikeData={
+        title:props.PostTitle,
+        content:props.PostContent,
+        date:props.PostDate,
+        like:!props.PostLiked
+    };
     return (
         <>
         <Card border="primary" style={{ width: '30rem' }} >
-            <Card.Header as="h4" >{props.PostDate}</Card.Header>
+            <Card.Header as="h4"  >{props.PostDate}</Card.Header>
             <Card.Body>
                 <Card.Title>{props.PostTitle}</Card.Title>
                 <Card.Text>
                     {props.PostContent}
                 </Card.Text>
-                <Button variant="secondary">Like</Button>
+                <Button disabled={props.PostLiked} variant="primary" onClick={()=>props.onLikes(props.Id,updateLikeData)}>Like</Button>
+               
             </Card.Body>
         </Card>
         <br/>
--- BlogApp/src/pages/PostPage.jsx
+++ BlogApp/src/pages/PostPage.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import ListPost from '../components/PostList';
-import ModalPost from '../components/ModalPost';
+import ModalPost from '../components/PostModal';
 
 function PostPage() {
     const [isLoading, setisLoading] = useState(false);
     const [isDataSent, setisDataSent] = useState(false);
+    const [postList, setpostList] = useState([]);
     // send Post request
     const handleSendRequest = async (data) => {
         setisLoading(true)
@@ -18,6 +19,40 @@ function PostPage() {
             console.log(error)
         }
     }
+    // send Put request
+    const handleLike = async (id,data) => {
+        setisLoading(true)
+        setisDataSent(false)
+        try {
+            const res = await axios.put(`https://react-http-1-f0a3b-default-rtdb.asia-southeast1.firebasedatabase.app/post_test/${id}.json`, data)
+            res.statusText === 'OK' && setisLoading(false);
+            res.statusText === 'OK' && setisDataSent(true);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    // fetch post list everytime new post sent
+    useEffect(() => {
+        async function fetchPostList() {
+            try {
+                const res = await axios.get('https://react-http-1-f0a3b-default-rtdb.asia-southeast1.firebasedatabase.app/post_test.json');
+                const posts = [];
+                for (const key in res.data) {
+                    const post = {
+                        id: key,
+                        ...res.data[key]
+                    };
+                    posts.push(post)
+                }
+                setpostList(posts)
+                console.log(posts)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPostList();
+    }, [isDataSent]);
+    
 
 
     return (
@@ -27,7 +62,8 @@ function PostPage() {
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
-            }}><ListPost onDataSent={isDataSent} /></div>
+                }}>
+                    <ListPost postList={postList} onLike={handleLike} /></div>
             
         </React.Fragment>
     )
